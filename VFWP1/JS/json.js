0acaa571e9a5c82d4418a4de35e9382c7134c171
Harry Lee <harrylee@Harry-Lees-MacBook-Pro.local>
2012-03-22 15:25:54 -0400

    made changes.

---
+++ VFWP1/JS/json.js
@@ -0,0 +1,32 @@
+var json = {
+	"book1": {
+		"group":	["Group:", 'Book'],
+		"author":	["Author:", "KingStephen"],
+		"title":	["Title:", "It"],
+		"books":	["Books:", "1"],
+		"favorite":	["Favorite:", "YES"],
+		"date":		["Date:", "2012-03-21"],
+		"checkbox":	["Checkbox:", "SciFi&Fantasy"],
+		"notes":	["Notes:", "Good"]
+	},
+		"book2": {
+		"group":	["Group:", 'Book'],
+		"author":	["Author:", "KingStephen"],
+		"title":	["Title:", "Mile"],
+		"books":	["Books:", "2"],
+		"favorite":	["Favorite:", "YES"],
+		"date":		["Date:", "2012-03-21"],
+		"checkbox":	["Checkbox:", "SciFi&Fantasy"],
+		"notes":	["Notes:", "Good"]
+	},
+		"book3": {
+		"group":	["Group:", 'Book'],
+		"author":	["Author:", "KingStephen"],
+		"title":	["Title:", "TheDarkTowerV"],
+		"books":	["Books:", "3"],
+		"favorite":	["Favorite:", "YES"],
+		"date":		["Date:", "2012-03-21"],
+		"checkbox":	["Checkbox:", "SciFi&Fantasy"],
+		"notes":	["Notes:", "V"]
+	}
+}
